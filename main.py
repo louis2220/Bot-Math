0e8dddb65d06cf4944906f58c85eff30fe058ba4
louis2220 <ph5652115@gmail.com>
2026-02-28 22:40:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def on_ready():
     await bot.change_presence(
         activity=discord.Activity(
             type=discord.ActivityType.watching,
-            name=f'{PREFIX}ajuda | Matemática 🔢'
+            name=f'{PREFIX}ajuda | Matemática'
         )
     )
 
